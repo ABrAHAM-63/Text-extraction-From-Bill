ed6bd8290319bbdd45a80825199c157d63cd15b7
ABrAHAM-63 <135248746+ABrAHAM-63@users.noreply.github.com>
2023-07-06 23:39:21 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import pytesseract as pyt
+import cv2
+import re
+
+img=cv2.imread("text.jpeg")
+
+
+text=pyt.image_to_string(img)
+
+pattern='GSTINIUIN: \w{15}'
+match=re.findall(pattern,text)
+print(match)
+
+
+mal='Total] = ([^\n]*)\)'
+total=re.findall(mal,text)
+print(total)
